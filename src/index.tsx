fc6a407a3c31fa0f3f521c4fe4f945b442173d38
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-05-13 20:29:40 -0300

    finalizou a Aula 2, importando arquivos estátivos

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import Footer from 'Components/Footer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Botao from './Components/Botao/Bota';
@@ -10,6 +11,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Cardapio />
-    <Botao></Botao>
+    {/* //componentes criados em exercício na aula, mas não utilizados */}
+    <Botao></Botao> 
+    <Footer/> 
   </React.StrictMode>
 );
