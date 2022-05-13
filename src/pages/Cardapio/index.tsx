a6dbc60fb859d011b294bd4d4797089613664518
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-05-13 20:10:10 -0300

    Importou arquivo estático como um ReactComponent

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from "../../assets/logo.svg";
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className={styles.menu}>
+                <Logo />
+            </nav>
+        </main>
+    )
+}
+
+//arquivos estáticos precisam ser importados para funcionarem
+//<img src="../../assets/logo.svg"> não funciona por isso
