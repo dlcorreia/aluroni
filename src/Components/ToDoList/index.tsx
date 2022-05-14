cafeab8b0da9f89af8c7057f63eae30ccb53d16a
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-05-14 12:54:11 -0300

    formatou o CSS da pagina Cardapio

--- src/Components/ToDoList/index.tsx
+++ src/Components/ToDoList/index.tsx
@@ -1,15 +1,13 @@
-import styles from './ToDoList.module.scss';
+import styles from 'Components/ToDoList/ToDoList.module.scss';
 
 
 export default function ToDoList() {
     return (
         <div className={styles.todo}>
             <ul>
-                <li className={styles.item}>criar o header e a div de texto na página Cardápio</li>
-                <li className={styles.item}>formatar o css dessa div</li>
-                <li className={styles.item}>importar a imagem de fundo</li>
-                <li className={styles.item}>utilizar uma variável de SASS para declarar o valor de padding</li>
-                <li className={styles.item}>separar as variáveis em um arquivo _variaveis.scss</li>
+
+                <li className={styles.item}></li>
+
               </ul>
         </div>
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css'; //alternativa para o reset pelo css
 import './index.css';
 import Cardapio from './pages/Cardapio';
-import Botao from './Components/Botao/Bota';
+import Botao from 'Components/Botao/Bota';
 import Footer from 'Components/Footer';
-import ToDoList from './Components/ToDoList';
+import ToDoList from 'Components/ToDoList';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
