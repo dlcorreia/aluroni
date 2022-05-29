2651d4c4e0d10add4910f072ca4f0ddb7ed785d8
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-05-29 19:22:23 -0300

    concluiu Atv_04 Usando react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css'; //alternativa para o reset pelo css
 import './index.css';
-import Cardapio from './pages/Cardapio';
 // import Botao from 'Components/Botao/Bota';
 // import Footer from 'Components/Footer';
 // import ToDoList from 'Components/ToDoList';
-import Inicio from 'pages/Inicio';
+import Router from './routes';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Inicio />
+    <Router />
     {/* <Cardapio /> */}
     {/* //componentes criados em exercício na aula, mas não utilizados */}
     {/* <ToDoList />
