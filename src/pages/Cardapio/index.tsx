a36d8769c12a34d95fd7d8096eb472c7c1684957
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-06-06 20:51:15 -0300

    concluiu Atv_07 Pagina Padrao

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -6,8 +6,6 @@ import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
 import Crescente from './Crescente';
-import Menu from 'Components/Menu';
-
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -16,36 +14,29 @@ export default function Cardapio() {
   const [ordem, setOrdem] = useState(true);
 
   return (
-    <main>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-                    A casa do codigo e da massa
+    <section className={styles.cardapio}>
+      <h3 className={styles.cardapio__titulo}>
+        Cardápio
+      </h3>
+      <Buscador
+        busca={busca}
+        setBusca={setBusca}
+      />
+      <div className={styles.cardapio__filtros}>
+        <div>
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
-      </header>
-      <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>
-                    Cardápio
-        </h3>
-        <Buscador
-          busca={busca}
-          setBusca={setBusca}
-        />
-        <div className={styles.cardapio__filtros}>
-          <div>
-            <Filtros filtro={filtro} setFiltro={setFiltro} />
+        <div className={styles.cardapio__filtros__ordenadores}>
+          <div className={styles.teste}>
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
           </div>
-          <div className={styles.cardapio__filtros__ordenadores}>
-            <div className={styles.teste}>
-              <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
-            </div>
-            <div className={styles.teste}>
-              <Crescente ordem={ordem} setOrdem={setOrdem} />
-            </div>
+          <div className={styles.teste}>
+            <Crescente ordem={ordem} setOrdem={setOrdem} />
           </div>
         </div>
-        <Itens busca={busca} filtro={filtro} ordenador={ordenador} ordem={ordem}/>
-      </section>
-    </main>
+      </div>
+      <Itens busca={busca} filtro={filtro} ordenador={ordenador} ordem={ordem} />
+    </section>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
 import Menu from 'Components/Menu';
+import PaginaPadrao from 'Components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
     <Router>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />}/>
-        <Route path="/cardapio" element={<Cardapio />}/>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="/cardapio" element={<Cardapio />} />
+        </Route>
       </Routes>
     </Router>
 
