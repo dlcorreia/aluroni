7e13d1c1be5070d1a2fdf8fc69edbb043b0bc7af
Daniel Correia <dlcorreia.ssa@gmail.com>
2022-06-07 21:59:47 -0300

    concluiu Atv_04 Terminando a tela Sobre

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,9 +1,31 @@
 import styles from './Sobre.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import casa from 'assets/sobre/casa.png';
+import massa1 from 'assets/sobre/massa1.png';
+import massa2 from 'assets/sobre/massa2.png';
+import texto from './texto.json';
+
+const imagens = [massa1, massa2];
 
 export default function Sobre() {
   return (
     <section>
-      Sobre
+      <h3 className={stylesTema.titulo}> Sobre </h3>
+      <div className={styles.sobreNos}>
+        <img src={casa} alt="Casa Aluroni"/>
+        <div className={styles.sobreNos__texto}>
+          {texto.map((item, index) => (
+            <p key={index}>{item.texto}</p>
+          ))}
+        </div>
+      </div>
+      <div className={styles.imagens}>
+        {imagens.map((imagem, index) => (
+          <div key={index} className={styles.imagens__imagem}>
+            <img src={imagem} alt="imagem de massa"/>
+          </div> 
+        ))}
+      </div>
     </section>
   );
 }
